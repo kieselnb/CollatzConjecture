6c29bcbe392aae66e09328988ac63ea9ea0be885
Nick Kiesel <kiesel.nicholas@gmail.com>
2018-03-25 22:26:02 -0400

    refactor so that don't have to prefill array sequentially

--- cuda/collatz.cu
+++ cuda/collatz.cu
@@ -11,26 +11,27 @@
 #include <signal.h>
 
 __global__
-void collatz(int n, long long unsigned int threshold, long long unsigned int *numbers, int* status) {
+void collatz(int n, long long unsigned int threshold, int* status) {
   int k = 2 * ((blockIdx.x * blockDim.x) + threadIdx.x);
 
-  // each thread gets two numbers - an odd and an even - to mitigate work imbalance
   if (k < n) {
+    // Use tid to get offset
+    long long unsigned int number = threshold + k + 1; // +1 since we took off one from currentnum to get threshold
+
     for (int j = 0; j < 2; j++) {
-      int i = k + j;
-      // we've already checked numbers <= threshold, so don't do those again
-      while (numbers[i] > 1 && numbers[i] > threshold) {
-        if (numbers[i] % 2 == 0)
-          numbers[i] = numbers[i] >> 1;
+      while (number > 1 && number > threshold) {
+        if (number % 2 == 0)
+          number = number >> 1;
         else
-          numbers[i] = ((numbers[i] * 3) + 1) >> 1;
+          number = ((number * 3) + 1) >> 1;
       }
-      if ((numbers[i] > 1) && (numbers[i] <= threshold) || numbers[i]==1)
-        numbers[i] = 1;
+      if ((number > 1 && number <= threshold) || (number == 1))
+        number = 1;
       else
         *status = 0;
     }
   }
+
 }
 
 bool keep_going;
@@ -60,10 +61,6 @@ int main(int argc, char* argv[]) {
   int N = 1<<21;
   struct timespec tp_start, tp_loopstart, tp_end;
   
-  long long unsigned int *numbers, *d_numbers;
-  // Initialize main memory array
-  numbers = (long long unsigned int *) malloc(N * sizeof(long long unsigned int));
-  
   // Create status variable space
   int status, *d_status;
   cudaError_t err = cudaMalloc(&d_status, sizeof(int));
@@ -72,15 +69,12 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  // Initialize GPU memory array
-  cudaMalloc(&d_numbers, N * sizeof(long long unsigned int));
-  
   // start main loop
-  int result = clock_gettime(CLOCK_REALTIME, &tp_start);
-  result = clock_gettime(CLOCK_REALTIME, &tp_loopstart);
+  clock_gettime(CLOCK_REALTIME, &tp_start);
+  clock_gettime(CLOCK_REALTIME, &tp_loopstart);
   while (keep_going) {
     // log timestamp and stats to console
-    result = clock_gettime(CLOCK_REALTIME, &tp_end);
+    clock_gettime(CLOCK_REALTIME, &tp_end);
     if (tp_end.tv_sec - tp_loopstart.tv_sec > 4) {
       std::stringstream timestamp;
       timestamp << "Uptime: ";
@@ -92,60 +86,30 @@ int main(int argc, char* argv[]) {
       timestamp << boost::str(boost::format("%12d") % currentNumber);
       timestamp << std::endl;
       std::cout << timestamp.str();
-      result = clock_gettime(CLOCK_REALTIME, &tp_loopstart);
+      clock_gettime(CLOCK_REALTIME, &tp_loopstart);
     }
-
-    // populate the array
-    for (int i = 0; i < N; i++) {
-      numbers[i] = currentNumber + i;
-    }
-    
-    // copy to device
-    cudaMemcpy(d_numbers, numbers, N*sizeof(long long unsigned int), cudaMemcpyHostToDevice);
 
     // initialize status to success
     status = 1;
     cudaMemcpy(d_status, &status, sizeof(int), cudaMemcpyHostToDevice);
     
     // perform collatz on entire array
-    collatz<<<((N/2)+255)/256, 256>>>(N, (currentNumber-1), d_numbers, d_status);
+    collatz<<<((N/2)+255)/256, 256>>>(N, (currentNumber-1), d_status);
     
-    // copy back to main mem
-    // cudaMemcpy(numbers, d_numbers, N*sizeof(long long unsigned int), cudaMemcpyDeviceToHost);
-
     // bring status back
     cudaMemcpy(&status, d_status, sizeof(int), cudaMemcpyDeviceToHost);
     
     // just check status variable
     if (status == 0) {
       std::cout << "Collatz failed" << std::endl;
-      // copy back to main mem and check where we failed
-      cudaMemcpy(numbers, d_numbers, N*sizeof(long long unsigned int), cudaMemcpyDeviceToHost);
-      for (int i = 0; i < N; i++) {
-        if (numbers[i] != 1) {
-          std::cout << "Collatz failed on: " << (currentNumber + i) << std::endl;
-          keep_going = false;
-          break;
-        }
-      }
       break;
     }
 
-    // check for errors (non-one values)
-    // for (int i = 0; i < N; i++) {
-    //   if (numbers[i] != 1) {
-    //     std::cout << "Collatz failed on number: " << (currentNumber + i) << std::endl;
-    //     keep_going = false;
-    //     break;
-    //   }
-    // }
     currentNumber += N;
   }
   
   std::cout << "While loop broken. Cleaning up..." << std::endl;
-  cudaFree(d_numbers);
   cudaFree(d_status);
-  free(numbers);
   
   return 0;
 }
