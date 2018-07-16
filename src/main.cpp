b7bfa9903ba0b530e4627a7e68630aeb98d72ad2
Nick Kiesel <kiesel.nicholas@gmail.com>
2018-07-16 17:59:18 -0500

    add boost program options parsing and doxygen doc generation

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,41 @@
 #include <vector>
 #include <mutex>
 #include <chrono>
+#include <boost/program_options.hpp>
 
 #include "collatz_counter.hpp"
 #include "collatz_runner.hpp"
 #include "collatz_runner_cpu.hpp"
 
 using namespace std;
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
+    // parse options
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help,h", "Display this message")
+        ("cpu", po::value<int>(), "Number of cpu threads to use")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // check if the user needs help. help the best way we know how
+    if (vm.count("help")) {
+        cout << desc << endl;
+        return 1;
+    }
+
+    int numProcs = thread::hardware_concurrency();
+    if (vm.count("cpu")) {
+        numProcs = vm["cpu"].as<int>();
+    }
+    cout << "Using " << numProcs << " local CPU compute threads." << endl;
+
     // shared counter and counter protector
     CollatzCounter collatzCounter;
-
-    // get number of cores
-    int numProcs = thread::hardware_concurrency();
-    cout << "I have " << numProcs << " threads available." << endl;
 
     // kick off runners for each core
     vector<CollatzRunner*> runners(numProcs);
@@ -37,6 +58,7 @@ int main(int argc, char* argv[]) {
     }
 
     while (true) {
+        // have some logic to check performance of the threads
         cout << "Current value of the counter is: " << collatzCounter.getCount()
             << endl;
         this_thread::sleep_for(chrono::seconds(10));
