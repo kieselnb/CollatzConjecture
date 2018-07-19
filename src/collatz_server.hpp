4e1a7ab9930509e48b3645ebec6d1ba5766d5fc6
Nick Kiesel <kiesel.nicholas@gmail.com>
2018-07-18 23:14:48 -0500

    add doxygen comments for server

--- src/collatz_server.hpp
+++ src/collatz_server.hpp
@@ -20,19 +20,62 @@
 class CollatzServer {
 
     public:
+        /**
+         * Constructor
+         *
+         * @param[in] counter Reference to The One Counter Object for clients
+         *                    to pull from.
+         * @param[in] port Port number to serve from.
+         */
         CollatzServer(CollatzCounter& counter, short port);
-        void start();
 
     private:
+        /**
+         * The One Counter Object to pull from.
+         */
         CollatzCounter& _counter;
+
+        /**
+         * Port number to serve from.
+         */
         short _port;
+
+        /**
+         * File descriptor of the listening port.
+         */
         int _fd;
+
+        /**
+         * Status on things in the constructor - make sure we have a valid
+         * file descriptor before calling more things on it.
+         */
         bool _initialized;
+
+        /**
+         * List of all the clients connected to me.
+         */
         std::list<int> _connections;
+
+        /**
+         * Thread that handles accepting new clients.
+         */
         std::thread _acceptor;
+
+        /**
+         * Thread that serves the connected clients.
+         */
         std::thread _poller;
+
+        /**
+         * Accepts new clients for the server.
+         */
         static void acceptor(int sockfd, std::list<int> &connections);
+
+        /**
+         * Loops through the connected clients and handles requests.
+         */
         static void poller(CollatzServer *server);
+
 };
 
 #endif  /* COLLATZ_SERVER_HPP */
